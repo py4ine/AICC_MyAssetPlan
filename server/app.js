25ecf506c7920b20440414a1100943c1917c52d0
py4ine <py.4ine@gmail.com>
2025-03-31 18:35:12 +0900

    fixing

--- server/app.js
+++ server/app.js
@@ -275,7 +275,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: '서버 오류가 발생했습니다.' });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
